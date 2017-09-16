eabb3b9f3e54553d1b575372a56e7ab67cd81cd9
Ekta Sorathia <ektasorathia@Ektas-MacBook-Pro.local>
2017-09-16 12:04:46 -0700

    Login Api created

---
+++ api/controllers/mysql.js
@@ -0,0 +1,38 @@
+const mysql= require("mysql");
+
+function getConnection()
+{
+    var connection = mysql.createConnection({
+        host     : 'mysql-cmpe280.c4lhvu8c7j2x.us-east-1.rds.amazonaws.com', //host where mysql server is running
+        user     : 'admin', //user for the mysql application
+        password : 'adminmysql', //password for the mysql application
+        database : 'cmpe280ikeamysql', //database name
+        port  : 3306 //port, it is 3306 by default for mysql  });
+    });
+    return connection;
+}
+
+
+function fetchData(callback,sqlQuery)
+{
+    console.log("\nSQL Query::"+sqlQuery);
+    var connection=getConnection();
+    connection.query(sqlQuery, function(err, rows, fields){
+            if(err)
+            {
+                console.log("ERROR: " + err.message);
+            }
+            else
+            { // return err or result
+                console.log("DB Results:"+rows);
+                callback(err, rows);
+
+            }
+
+        }
+    );
+    console.log("\nConnection closed..");
+    connection.end();
+
+}
+exports.fetchData= fetchData;
--- api/controllers/users_ctrl.js
+++ api/controllers/users_ctrl.js
@@ -1,21 +1,43 @@
 'use strict';
 
 let util = require('util');
-
+const crypto = require('crypto');
+var mysql = require('./mysql');
 module.exports = {
     doLogin
 };
 
+
 function doLogin(req, res) {
     let reqData = req.swagger.params.body.value;
 
-    let username = reqData.username;
+    let email = reqData.email;
     let password = reqData.password;
+    let loginSucess = false;
+   	let getLoginDetails = "SELECT * FROM cmpe280ikeamysql.users WHERE email_id='" + email + "' LIMIT 1";
+   	console.log(process.env);
+    mysql.fetchData(function (err, results) {
+    	
+        if (err) {
+            console.log("Error: "+err);
+            res.code=401;
+            callback(null, res);
+        }else if(results.length>0) {
+        	
+        	let passwordDB = results[0].password;
+        	let saltDB = results[0].salt;
+        		//let passwordIN = crypto.pbkdf2Sync(password, saltDB, 100000, 64, 'sha256').toString('hex');
+        	if (password == passwordDB) {
+          		loginSucess = true;
+          		res.status(200).json({message: "Login done."})
+        	}
 
-
-    username === "admin"
-    && password === "admin"
-    && res.status(200).json({message: "Login done."})
-    || res.status(401).json({message: "Login Failed."});
-
+        	if (!loginSucess) {
+        		console.log("invalid login");
+        		res.status(401).json({message: "Login Failed."});
+        		//res.send(jsonResponse);
+      		}
+        }
+    },getLoginDetails);
 }
+
