65ffc537c8d9c7db865f019f9d1703831b879f51
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-09-17 21:58:28 -0700

    401 response in login

--- api/controllers/users_ctrl.js
+++ api/controllers/users_ctrl.js
@@ -13,31 +13,30 @@ function doLogin(req, res) {
 
     let email = reqData.email;
     let password = reqData.password;
-    let loginSucess = false;
-   	let getLoginDetails = "SELECT * FROM cmpe280ikeamysql.users WHERE email_id='" + email + "' LIMIT 1";
-   	console.log(process.env);
+    let loginSuccess = false;
+    let getLoginDetails = "SELECT * FROM cmpe280ikeamysql.users WHERE email_id='" + email + "' LIMIT 1";
+    console.log(process.env);
     mysql.fetchData(function (err, results) {
-    	
-        if (err) {
-            console.log("Error: "+err);
-            res.code=401;
-            callback(null, res);
-        }else if(results.length>0) {
-        	
-        	let passwordDB = results[0].password;
-        	let saltDB = results[0].salt;
-        		//let passwordIN = crypto.pbkdf2Sync(password, saltDB, 100000, 64, 'sha256').toString('hex');
-        	if (password == passwordDB) {
-          		loginSucess = true;
-          		res.status(200).json({message: "Login done."})
-        	}
 
-        	if (!loginSucess) {
-        		console.log("invalid login");
-        		res.status(401).json({message: "Login Failed."});
-        		//res.send(jsonResponse);
-      		}
+        if (err) {
+            console.log("Error: " + err);
+            res.code = 401;
+            callback(null, res);
+        } else {
+            if (results.length > 0) {
+
+                let passwordDB = results[0].password;
+                let saltDB = results[0].salt;
+                //let passwordIN = crypto.pbkdf2Sync(password, saltDB, 100000, 64, 'sha256').toString('hex');
+                if (password == passwordDB) {
+                    loginSuccess = true;
+                    res.status(200).json({message: "Login done."})
+                }
+
+            } else {
+                res.status(401).json({message: "Login Failed."});
+            }
         }
-    },getLoginDetails);
+    }, getLoginDetails);
 }
 
